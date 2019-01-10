4eaa2acbab212b2fefbe7d4489ea39d1bfb7ac54
ArcNgen <robert@arc-ingen.com>
2019-01-10 08:12:18 -0500

    added dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { QcDashboardComponent } from './components/qc-dashboard/qc-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    QcDashboardComponent
   ],
   imports: [
     BrowserModule,
