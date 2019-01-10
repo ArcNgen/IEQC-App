80aa514ac8e9f348e70a4cbe96d74ac958e04f2e
ArcNgen <robert@arc-ingen.com>
2019-01-10 08:19:02 -0500

    updated routing to load dashboard and added link to landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { LandingPageComponent } from "./components/landing-page/landing-page.component";
+import { QcDashboardComponent } from "./components/qc-dashboard/qc-dashboard.component";
 
 const routes: Routes = [
   {
     path: '',
     component: LandingPageComponent
+  },
+  {
+    path: 'dashboard',
+    component: QcDashboardComponent
   }
 ];
 
