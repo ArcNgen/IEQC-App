5ced0133c537b9b8d030b86138412553b23892ca
ArcNgen <robert@arc-ingen.com>
2019-01-11 05:51:45 -0500

    added chart to qc dashboard with api call data source

--- src/app/components/qc-dashboard/qc-dashboard.component.ts
+++ src/app/components/qc-dashboard/qc-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { QcTestDataService } from "../../qc-test-data.service";
+
 import { Chart } from "chart.js";
 
 @Component({
@@ -8,6 +9,7 @@ import { Chart } from "chart.js";
   styleUrls: ['./qc-dashboard.component.scss']
 })
 export class QcDashboardComponent implements OnInit {
+  chart: [];
 
   constructor(
     private dataService: QcTestDataService
@@ -15,7 +17,55 @@ export class QcDashboardComponent implements OnInit {
 
   ngOnInit() {
     this.dataService.dailyForcast().subscribe(res => {
-      console.log('res', res);
+      console.log(res);
+      
+      let temp_max = res['list'].map(res => res.main.temp_max);
+      let temp_min = res['list'].map(res => res.main.temp_min);
+      let alldates = res['list'].map(res => res.dt);
+      let weatherDates = [];
+      // let canvasId = document.getElementById("canvas");
+      // let ctx = document.getElementById("canvas");
+
+      alldates.forEach((res) => {
+        let jsdate = new Date(res * 1000);
+        weatherDates.push(jsdate.toLocaleTimeString('en', {year: 'numeric', month: 'short', day: 'numeric'}));
+      });
+
+      
+
+      this.chart = new Chart('myCanvas', {
+        type: 'line',
+        data: {
+          labels: weatherDates,
+          datasets: [
+            {
+              data: temp_max,
+              borderColor: '#3cba9f',
+              fill: false
+            },
+            {
+              data: temp_min,
+              borderColor: '#ffcc00',
+              fill: false
+            }
+          ]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          scales: {
+            xAxis: [{
+              display: true
+            }],
+            yAxis: [{
+              display: true
+            }]
+          }
+        }
+      });
+
+      // console.log(this.chart); 
       
     });
   }
