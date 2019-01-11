0bb5661c90f1b03a02ee22fed2b09249a1725cd5
ArcNgen <robert@arc-ingen.com>
2019-01-11 10:16:12 -0500

    created new dashboard component

---
+++ src/app/components/qc-dashboard-v2/qc-dashboard-v2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qc-dashboard-v2',
+  templateUrl: './qc-dashboard-v2.component.html',
+  styleUrls: ['./qc-dashboard-v2.component.scss']
+})
+export class QcDashboardV2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
